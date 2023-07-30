8cf985167934f75447419cca8eaa0b5139d91cc1
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-07-30 12:26:56 +0300

    Import Roboto font family

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -23,37 +23,45 @@
 </template>
 
 <style lang="css">
-  body {
-   font-family: Roboto; 
-  }
-  #navbar {
-    max-width: 1440px;
-    margin: 0 auto;
-    min-height: 64px;
-    display: flex;
-    align-items: center;
-  }
-  #navbar-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    max-width: 1376px;
-    width: 100%;
-    margin: 0 auto;
-  }
-  .navbar-link-container {
-    display: flex;
-    gap: 1rem;
-  }
-  #navbar-middle-link {
-    position: relative;
-    left: 59px;
-  }
-  a {
-    text-decoration: none;
-    color: #FF4D00;
-    font-size: 24px; 
-  }
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+#navbar {
+  max-width: 1440px;
+  margin: 0 auto;
+  min-height: 64px;
+  display: flex;
+  align-items: center;
+}
+
+#navbar-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1376px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.navbar-link-container {
+  display: flex;
+  gap: 1rem;
+}
+
+#navbar-middle-link {
+  position: relative;
+  left: 59px;
+}
+
+a {
+  text-decoration: none;
+  color: #FF4D00;
+  font-weight: 500;
+  font-size: 24px;
+}
 </style>
 
 <script>
