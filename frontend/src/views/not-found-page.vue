0856efa30d264da1d7144379140640015c63b629
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-12 22:18:37 +0300

    Create not found page

---
+++ frontend/src/views/not-found-page.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.not-found-container
+    NotFound404
+    p.not-found-explanation I guess what you are looking for is not here &nbsp;&nbsp;-Loky
+</template>
+<script>
+import NotFound404 from '../components/not-found-404.vue';
+export default {
+    name: "NotFoundPage",
+    components: {
+        NotFound404
+    }
+}
+</script>
+<style lang="css">
+.not-found-container {
+    position: relative;
+    top: 167px;
+    margin: 0 auto;
+    width: 700px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
+.not-found-explanation {
+    font-size: 16px;
+    line-height: 19.2px;
+    color: #FF4D0080;
+    margin-top: 64px;
+
+}
+</style>
