0e793a588ab7f35b11b6d8eca99c218781eb8c60
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-12 22:19:47 +0300

    Create outlined input component

---
+++ frontend/src/components/outlined-input.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+div.input-container
+    input.input-text(:placeholder="placeholder" :type="type")
+    div
+</template>
+<script>
+export default {
+    name: "OutlinedInput",
+    props: {
+        placeholder: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+<style lang="css">
+.input-text {
+    width: 443px;
+    height: 34px;
+    border: none;
+    border-bottom: 3px solid #F5D5C6;
+    padding: 6px 0px 8px 5px;
+    box-sizing: border-box;
+    color: #FF4D00;
+    font-size: 21px;
+    line-height: 28.8px;
+}
+
+.input-text::placeholder {
+    font-size: 21px;
+    color:#F5D5C6;
+    line-height: 28.8px;
+}
+
+.input-text + div {
+    content: "";
+    width: 0;
+    display: block;
+    height: 3px;
+    background-color: #FF4D00;
+    transition: all 0.3s ease-in;
+    position: relative;
+    top: -3px;
+}
+.input-text:focus {
+    outline: none;
+}
+.input-text:focus + div {
+    width: 443px;
+}
+</style>
