dace41e23a48c9798ba27965e70df6b2ceceec00
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-12 23:52:02 +0300

    Fix css and configure the code for logo sizing in contact page

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -5,42 +5,67 @@
     #navbar-box
       #navbar-container
         #navbar-logo 
-          Logo(
-            width=45
-            height=36
-          )
+          Logo(:logoSize="logoSize")
         .navbar-link-container#navbar-middle-link
           .navbar-link-item 
             router-link(to='/products') Products
           .navbar-link-item 
             router-link(to='/cargo') Cargo
           .navbar-link-item 
-            router-link(to='/contact') Contact
+            router-link(to='/contact') 
+              span(@click.navtive="updateLogoSize") Contact
         .navbar-link-container
           .navbar-link-item 
             router-link(to='/sign-in') Sign in
           .navbar-link-item 
             router-link(to='/cart/:id') Cart(0)
-
-
   router-view
 </template>
+
+<script>
+import Logo from './components/logo.vue'
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  components: {
+    Logo,
+  },
+  setup(props) {
+    const store = useStore()
+    const logoSize = ref(false)
+
+    const updateLogoSize = () => {
+      store.commit('updateLogoSize', true)
+    }
+
+    return {
+      logoSize,
+      updateLogoSize,
+    }
+  },
+}
+</script>
 
 <style lang="css">
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap');
 
 body {
   font-family: 'Roboto', sans-serif;
-  color: #FF4D00;
+  color: #ff4d00;
 }
 
+:root {
+  --bg-textColor-secondary: #2d2d2d;
+  --bg-textColor-thirth: #7b7b7b;
+}
 #navbar {
   min-height: 64px;
   display: flex;
   align-items: center;
   position: relative;
   z-index: 9;
-  background: white;
+  background: transparent;
   width: 100%;
 }
 
@@ -83,12 +108,3 @@ body {
   top: 13px;
 }
 </style>
-
-<script>
-import Logo from './components/logo.vue';
-export default {
-  components: {
-    Logo,
-  }
-}
-</script>
