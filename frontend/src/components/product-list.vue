daa01b93aab0582d0a7d24f2496afb01363cb8c9
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 12:14:02 +0300

    Initialize list to display products

---
+++ frontend/src/components/product-list.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+div.products-container#list(ref="dragWrapperCarosuel")
+        ProductQuarter
+        ProductQuarter
+        ProductQuarter
+        ProductQuarter
+        ProductQuarter
+        ProductQuarter
+        ProductQuarter
+        ProductQuarter
+
+</template>
+<script>
+import ProductQuarter from './product-quarter.vue'
+import { ref, onMounted } from 'vue'
+export default {
+  name: 'ProductList',
+  components: {
+    ProductQuarter,
+  },
+  props: {
+    getRef: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  setup(props) {
+    const dragWrapperCarosuel = ref(null)
+    onMounted(() => {
+      props.getRef(dragWrapperCarosuel.value)
+    })
+    return {
+      dragWrapperCarosuel,
+    }
+  },
+}
+</script>
+<style lang="css">
+.minimap .products-container {
+  gap: 4.8px !important;
+  margin: 50px auto 0 !important;
+  width: auto !important;
+  margin-left: 6.1px;
+}
+
+.products-container {
+  display: flex;
+  gap: 24px;
+  margin-left: 32px;
+  position: relative;
+  top: 37px;
+  width: calc(100% - 32px);
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+
+.products-container::-webkit-scrollbar {
+  display: none;
+}
+</style>
