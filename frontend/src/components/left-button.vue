4ccd79b85af0b82ffbaa9fd869b912ca041539b1
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 11:57:15 +0300

    Initialize left arrow svg icon component of button

---
+++ frontend/src/components/left-button.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 12H18" stroke="#FF4D00" stroke-width="1.5" />
+        <path d="M10.5 7.5L6 12L10.5 16.5" stroke="#FF4D00" stroke-width="1.5" />
+    </svg>
+</template>
+<script>
+export default {
+    name: 'LeftArrow'
+}
+</script>
+<style lang="">
+    
+</style>
