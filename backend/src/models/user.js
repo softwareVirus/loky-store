caddb38fef1d387bfe976c823d09486847202cbf
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 15:25:32 +0300

    Create user mongodb schema

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,16 +1,53 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
+const UserSchema = new mongoose.Schema(
+    {
+      firstName: {
+        type: String,
+        required: true
+      },
+      lastName: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      salt: {
+        type: String,
+        required: true
+      },
+      orderItems: [
+        {
+          orderCard: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'OrderCard',
+          },
+          quantity: {
+            type: Number,
+            required: true
+          },
+          orderDate: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
     },
-  },
-  { timestamps: true }
-)
+    { timestamps: true } // This option adds created_at and updated_at fields
+  )
 
-userSchema.plugin(autopopulate)
+UserSchema.plugin(autopopulate)
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', UserSchema)
