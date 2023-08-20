00f920962166c5378f284b32011b8a57e397ea40
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 12:15:43 +0300

    Initialize right arrow svg icon

---
+++ frontend/src/components/right-arrow.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <svg
+    width="12"
+    height="11"
+    style="margin-left: 5px"
+    viewBox="0 0 12 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6.83333 1.125L11 5.5M11 5.5L6.83333 9.875M11 5.5L1 5.5"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="square"
+      stroke-linejoin="bevel"
+    />
+  </svg>
+</template>
+<script>
+export default {
+  name: 'RightArrow',
+}
+</script>
+<style lang=""></style>
