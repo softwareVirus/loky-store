f9061a578338ac969ff0f547d37796690c14cc0f
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 11:18:16 +0300

    Initialize the empty basket svg icon

---
+++ frontend/src/components/empty-basket-icon.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M15.3333 20.6667H48.6667M15.3333 20.6667V48.6667H48.6667V20.6667M15.3333 20.6667L20.6667 12.6667H43.3333L48.6667 20.6667"
+            stroke="#FF4D00" stroke-width="3" />
+        <path d="M38 26.6667V29.3334C38 32.6401 35.3067 35.3334 32 35.3334C28.6667 35.3334 26 32.6401 26 29.3334V26.6667"
+            stroke="#FF4D00" stroke-width="3" />
+    </svg>
+</template>
+<script>
+export default {
+    name: "EmptyBasketIcon"
+}
+</script>
+<style lang="">
+    
+</style>
