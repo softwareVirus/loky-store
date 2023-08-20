27ed4471601a74f5372fc736511ee78faee9bb04
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 13:00:23 +0300

    Add logoSize state and its mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,8 +2,20 @@ import { createStore } from 'vuex'
 import axios from 'axios'
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    logoSize: false,
+  },
+  getters: {
+    logoSize(state) {
+      return state.logoSize
+    },
+  },
+  mutations: {
+    updateLogoSize(state, newValue) {
+      console.log(newValue)
+      state.logoSize = newValue
+    },
+  },
   actions: {
     async fetchUsers() {
       return (await axios.get('/api/users')).data
