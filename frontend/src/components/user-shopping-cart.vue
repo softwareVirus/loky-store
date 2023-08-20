746cc539305f8cfd276934414ce663b4f8fa5335
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 12:19:23 +0300

    Initialize shopping cart page

---
+++ frontend/src/components/user-shopping-cart.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+EmptyShoppingCart
+ShoppingCartFooter
+</template>
+<script>
+import EmptyShoppingCart from './empty-shopping-cart.vue';
+import ShoppingCartFooter from './shopping-cart-footer.vue';
+export default {
+    name: 'UserShoppingCart',
+    components: {
+        EmptyShoppingCart,
+        ShoppingCartFooter
+    }
+}
+</script>
+<style lang="">
+    
+</style>
