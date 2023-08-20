c85dd81555705bd98464cccd43df2dea505f9bbf
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 11:20:25 +0300

    Initialize empty cart to inform the user

---
+++ frontend/src/components/empty-shopping-cart.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+.empty-basket 
+    .empty-basket-box 
+        EmptyBasketIcon
+        h2.empty-basket-title Your cart is empty 
+        p.empty-basket-content You can examine our carefully prepared#[<br>]products on the products page!
+        PrimaryButton(content="Back to shop")
+</template>
+<script>
+import EmptyBasketIcon from './empty-basket-icon.vue';
+import PrimaryButton from './primary-button.vue';
+export default {
+    name: 'EmptyShoppingCart',
+    components: {
+        EmptyBasketIcon,
+        PrimaryButton
+    }
+}
+</script>
+<style lang="css">
+    .empty-basket {
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        top: 0;
+        left: 0;
+    }
+    .empty-basket-title {
+        margin: 16px 0 0 0;
+    }
+    .empty-basket-content {
+        text-align: center;
+        margin: 12px 0 22px 0;
+        color: #FF4D0080;
+    }
+    .empty-basket-box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
