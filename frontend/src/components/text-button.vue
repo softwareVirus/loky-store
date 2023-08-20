5568ddae7dc4f334dae7e6effb67b2d1e960e0c7
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 12:17:46 +0300

    Initialize filled button

---
+++ frontend/src/components/text-button.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+router-link#text-button(:to="direction") {{buttonContent}}
+</template>
+<script>
+export default {
+  name: 'TextButton',
+  props: {
+    buttonContent: {
+      type: String,
+      required: true,
+    },
+    direction: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+<style lang="css">
+.minimap #text-button {
+  line-height: 4.4px;
+  font-size: 2.8px;
+}
+#text-button {
+  color: inherit;
+  line-height: 22px;
+  font-size: 14px;
+  text-decoration: none;
+}
+</style>
