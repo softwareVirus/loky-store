53b4eadc3123d4d1f31d55333650a7645a74f68a
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-20 11:58:13 +0300

    Initialize primary button component that don't have background and border

---
+++ frontend/src/components/primary-button.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+button.primary-button #[LeftArrow]{{ content  }}
+</template>
+<script>
+import LeftArrow from './left-button.vue'
+export default {
+    name: 'PrimaryButton',
+    props: {
+        content: {
+            type: String,
+            required: true
+        }
+    },
+    components: {
+        LeftArrow
+    }
+}
+</script>
+<style lang="css">
+.primary-button {
+    background-color: white;
+    border: none;
+    color: #FF4D00;
+    line-height: 19.2px;
+    font-weight: 500;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 0;
+}
+</style>
