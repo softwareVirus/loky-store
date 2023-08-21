968476507a869be689bb01e175687e9debcdab6d
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-21 13:51:14 +0300

    Add cart products state with its mutations and actions to add and remove

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,19 +4,34 @@ import axios from 'axios'
 export default createStore({
   state: {
     logoSize: false,
-  },
-  getters: {
-    logoSize(state) {
-      return state.logoSize
-    },
+    cartProducts: []
   },
   mutations: {
     setLogoSize(state, newValue) {
       state.logoSize = newValue
     },
+    setProducts(state, products) {
+      state.cartProducts = products
+    } 
   },
   actions: {
-    updateLogoSize: ({ commit }, newLogoSize) => commit('updateLogoSize', newLogoSize),
+    updateLogoSize({ commit }, newLogoSize){
+       commit('updateLogoSize', newLogoSize)
+    },
+    addProductToCart({ commit, state }, product) {
+      if(state.cartProducts.map(item => item.id).includes(product.id)) {
+        return false
+      }
+      commit("setProducts", [...state.cartProducts, product]);
+      return true;
+    },
+    removeProductFromCart({ commit, state }, product) {
+      if(state.cartProducts.map(item => item.id).includes(product.id) == false) {
+        return false
+      }
+      commit("setProducts", state.cartProducts.filter(item => item.id != product.id))
+      return true;
+    },
     async fetchUsers() {
       return (await axios.get('/api/users')).data
     },
