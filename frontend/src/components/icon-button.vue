bad26d1ebc5f87411a783f00d0a1a8c0f1f0c46f
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-21 15:06:40 +0300

    Initialize icon button component

---
+++ frontend/src/components/icon-button.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+button(:class="'icon-button ' + (isCloseButton ? 'close-button' : 'favorite-button')" @click.native="buttonClickEvent")
+    CloseIcon(v-if="isCloseButton")
+    FavoriteIcon(v-else)
+</template>
+<script>
+import CloseIcon from './close-icon.vue';
+import FavoriteIcon from './order-card/favorite-icon.vue';
+export default {
+    name: "IconButton",
+    components: {
+        CloseIcon,
+        FavoriteIcon
+    },
+    props: {
+        isCloseButton: {
+            type: Boolean,
+            default: false
+        },
+        buttonClickEvent: {
+            type: Function,
+            required: true,
+            default: () => {}
+        }
+    }
+}
+</script>
+<style lang="">
+.icon-button {
+    padding: 8px
+}    
+</style>
