2be67ffd2c3082071da781d0a95e389bdbf61e87
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-21 14:57:16 +0300

    Initialize close icon as component

---
+++ frontend/src/components/close-icon.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M21.9203 12.0208L12.0208 21.9202" stroke="white" stroke-width="1.5" />
+        <path d="M12.0209 12.0208L21.9204 21.9202" stroke="white" stroke-width="1.5" />
+    </svg>
+</template>
+<script>
+export default {
+    name: "CloseIcon"
+}
+</script>
+<style lang="">
+    
+</style>
