1abc6b02f66b723d7dfb03fa94f784fbf0eed616
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-25 17:17:49 +0300

    Add close icon feature to icon button && Add hover to button

--- frontend/src/components/icon-button.vue
+++ frontend/src/components/icon-button.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
 button(:class="'icon-button ' + (isCloseButton ? 'close-button' : 'favorite-button')" @click.native="buttonClickEvent")
-    CloseIcon(v-if="isCloseButton")
+    PlusIcon(v-if="isCloseButton" className="close-icon" color="white")
     FavoriteIcon(v-else)
 </template>
 <script>
-import CloseIcon from './close-icon.vue';
+import PlusIcon from './order-card/plus-icon.vue';
 import FavoriteIcon from './order-card/favorite-icon.vue';
 export default {
     name: "IconButton",
     components: {
-        CloseIcon,
+        PlusIcon,
         FavoriteIcon
     },
     props: {
@@ -25,8 +25,28 @@ export default {
     }
 }
 </script>
-<style lang="">
+<style lang="css">
 .icon-button {
-    padding: 8px
+    padding: 4px;
+    max-height: 2rem;
+    border: none;
+    cursor: pointer;
 }    
+
+.close-button {
+    background-color: var(--bg-color-primary);
+    color: white;
+}
+
+.favorite-button {
+    background-color: white;
+}
+
+.close-button:hover {
+    background-color: #B13600;
+}
+
+.favorite-button:hover {
+    background-color: rgb(231 231 231);
+}
 </style>
