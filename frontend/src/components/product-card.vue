a737fe5078ed67c559fcc79fa8136a2810dd35d8
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-25 17:23:05 +0300

    Edit cart to use same component in user basket feature

--- frontend/src/components/product-card.vue
+++ frontend/src/components/product-card.vue
@@ -1,17 +1,31 @@
 <template lang="pug">
-.product-card
-    img.product-image(src="@/assets/Untitled.png")
-    .product-card-info
-        p.product-title {{productTitle}}
-        p.product-cost {{productCost}}
-        TextButton(buttonContent="ADD TO CART" direction="/products/:id")
+div(:class="productCost == null ? 'product-shopping-card' : 'product-card'")
+  div(:class="productCost == null ? 'product-header product-shopping-header' : 'product-header'")
+    .product-buttons-container(v-if="productCost == null")
+      .product-buttons 
+        IconButton
+        IconButton(:isCloseButton="true")
+    img.product-image(src="@/assets/Untitled.png" v-if="productCost != null")
+  .product-card-info
+    p.product-title {{productTitle}}
+    p.product-cost(v-if="productCost != null") {{productCost}}
+    p.product-cost(v-else) {{productSize}}
+    TextButton(buttonContent="ADD TO CART" direction="/products/:id" v-if="productCost != null")
+    .quantity-buttons(v-else)
+      IconButtonNoPadding
+      p.product-cost {{ 2 }}
+      IconButtonNoPadding(:isPlusIcon="true")
 </template>
 <script>
 import TextButton from './text-button.vue'
+import IconButton from './icon-button.vue';
+import IconButtonNoPadding from './icon-button-no-padding.vue';
 export default {
   name: 'ProductCard',
   components: {
     TextButton,
+    IconButton,
+    IconButtonNoPadding
   },
   props: {
     productTitle: {
@@ -20,8 +34,12 @@ export default {
     },
     productCost: {
       type: String,
-      required: true,
+      default: null
     },
+    productSize: {
+      type: String,
+      default: ""
+    }
   },
 }
 </script>
@@ -45,16 +63,31 @@ export default {
   gap: 4.8px !important;
 }
 
+.quantity-buttons {
+  display: flex;
+  gap: 0.25rem;
+  align-items: center;
+}
+
 .product-image {
   width: 430px;
+  position: relative;
+  top: 0;
 }
 
 .product-title,
 .product-cost {
   color: inherit;
-  line-height: 22px;
+  line-height: 23px;
   font-size: 14px;
   margin: 0;
+}
+
+.product-shopping-header {
+  height: 382px !important;
+  background-image: url("~@/assets/Untitled.png");
+  background-size: 350px !important;
+  width: 350px !important;
 }
 
 .product-card-info {
@@ -68,5 +101,25 @@ export default {
   flex-direction: column;
   gap: 3px;
   width: 430px;
+}
+
+.product-shopping-card {
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+  width: 350px;
+}
+
+.product-buttons-container {
+  position: relative;
+  display: flex;
+  margin-top: 0.5rem;
+  margin-right: 0.5rem;
+  justify-content: flex-end;
+}
+
+.product-buttons {
+  display: flex;
+  gap: 0.5rem;
 }
 </style>
