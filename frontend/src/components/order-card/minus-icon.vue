5b6ffe2a6dc3e8d594471c6d4e4f5f344a770d14
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-25 17:27:58 +0300

    Add className and color props to control stroke attribute and css of svg

--- frontend/src/components/order-card/minus-icon.vue
+++ frontend/src/components/order-card/minus-icon.vue
@@ -1,11 +1,21 @@
 <template lang="html">
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M6 12H18" stroke="#2D2D2D" stroke-width="1.5" />
+    <path d="M6 12H18" :stroke="color" stroke-width="1.5" />
   </svg>
 </template>
 <script>
 export default {
   name: 'MinusIcon',
+  props: {
+    color: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ""
+    }
+  }
 }
 </script>
 <style lang=""></style>
