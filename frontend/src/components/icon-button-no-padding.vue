e0b4ac163289ba8ae667c3755d6bf53b48d62252
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-08-25 17:15:44 +0300

    Add icon button for carts of user-basket feature

---
+++ frontend/src/components/icon-button-no-padding.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+button.icon-button-v2 
+    PlusIcon(color="#ff4d00" v-if="isPlusIcon")
+    MinusIcon(color="#ff4d00" v-else)
+</template>
+<script>
+import PlusIcon from './order-card/plus-icon.vue';
+import MinusIcon from './order-card/minus-icon.vue';
+export default {
+    name: "IconButtonNoPadding",
+    props: {
+        isPlusIcon: {
+            type: Boolean,
+            default: false
+        }
+    },
+    components: {
+        PlusIcon,
+        MinusIcon
+    }
+}
+</script>
+<style lang="css">
+.icon-button-v2 {
+    background-color: white;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    height: 1.5rem;
+    transition: all 0.1s ease-in;
+}
+
+.icon-button-v2:hover {
+    background-color: rgb(231 231 231);
+}
+</style>
